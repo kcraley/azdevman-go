954f73823164dc2413566271b5ad5946cce78c1a
Keith Craley <kcral21@gmail.com>
2020-04-18 10:49:35 -0400

    remove viper for configuration

--- cmd/root.go
+++ cmd/root.go
@@ -4,10 +4,8 @@ import (
 	"os"
 
 	"github.com/microsoft/azure-devops-go-api/azuredevops"
-	homedir "github.com/mitchellh/go-homedir"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 // Default values for flags
@@ -30,14 +28,6 @@ var (
 		Short: "azdevman manages instances of Azure DevOps",
 		Long:  "azdevman is a simple CLI tool to interact with Microsoft's Azure DevOps.",
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			log.SetOutput(os.Stdout)
-			// Set verbose logging if true
-			if verbose {
-				log.SetLevel(log.DebugLevel)
-			} else {
-				log.SetLevel(log.InfoLevel)
-			}
-
 			// Create a connection object everytime
 			connection = azuredevops.NewPatConnection(azureDevOpsURL+organization, token)
 		},
@@ -46,10 +36,10 @@ var (
 
 func init() {
 	// Initialize Viper config on execution
-	cobra.OnInitialize(initViperConfig)
+	cobra.OnInitialize(initConfig)
 
 	// Root level flags for the cli
-	rootCmd.PersistentFlags().StringVarP(&configFile, "config", "c", defaultConfigFile, "config file")
+	rootCmd.PersistentFlags().StringVarP(&configFile, "config", "c", "", "config file")
 	rootCmd.PersistentFlags().StringVarP(&organization, "organization", "o", "", "sets the Azure DevOps organization to connect to")
 	rootCmd.PersistentFlags().StringVarP(&token, "token", "t", "", "the personal access token used to connect to the organization")
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "enable verbose logging by including additional output")
@@ -57,19 +47,13 @@ func init() {
 
 // initViperConfig is responsible for configuring Viper
 // anytime each command's Execute method is called
-func initViperConfig() {
-	// Set the config path location
-	home, err := homedir.Expand(configFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	viper.SetConfigFile(home)
-
-	// Read in all environment variables that match
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err == nil {
-		log.Println("Using config file: ", viper.ConfigFileUsed())
+func initConfig() {
+	// Configure logging
+	log.SetOutput(os.Stdout)
+	log.SetFormatter(&log.JSONFormatter{})
+	// Set verbose logging if true
+	if verbose {
+		log.SetLevel(log.DebugLevel)
 	}
 }
 
