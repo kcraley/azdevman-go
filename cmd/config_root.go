457b91c1b569f56bae25e4cf1a71aae4e982fb54
Keith Craley <kcral21@gmail.com>
2020-04-19 13:28:37 -0400

    add basic scaffolding for the config subcommand

--- cmd/config_root.go
+++ cmd/config_root.go
@@ -1,1 +1,13 @@
 package cmd
+
+import "github.com/spf13/cobra"
+
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Manage the configuration to connect to various Azure DevOps organizations or projects.",
+}
+
+func init() {
+	// Add `config` subcommand to `azdevman`
+	rootCmd.AddCommand(configCmd)
+}
