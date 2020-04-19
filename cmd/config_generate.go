4333ceedf5ea7491f267c55d2bffe37fc4c8b3f6
Keith Craley <kcral21@gmail.com>
2020-04-19 13:30:04 -0400

    add basic scaffolding to create a new configuration context

--- cmd/config_generate.go
+++ cmd/config_generate.go
@@ -1,1 +1,25 @@
 package cmd
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+// configGenerateCmd represents the generate subcommand
+var configGenerateCmd = &cobra.Command{
+	Use:     "generate",
+	Short:   "Generate a new configuration context connection.",
+	Example: `  azdevman config generate`,
+	RunE:    configGenerateFunc,
+}
+
+func init() {
+	// Add `generate` subcommand to `config`
+	configCmd.AddCommand(configGenerateCmd)
+}
+
+// configCurrentContextFunc is the main entrypoint for `azdevman config generate`
+func configGenerateFunc(cmd *cobra.Command, args []string) error {
+	log.Info("azdevman config generate was called")
+	return nil
+}
